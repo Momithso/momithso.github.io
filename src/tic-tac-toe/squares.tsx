223e90df3a33e702e234e07731eafd50670af28b
Maurice Sonntag <mts.sonntag@gmail.com>
2020-09-28 08:47:42 +0200

    github-pages

--- src/tic-tac-toe/squares.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Square(props: any, i: number) {
-    return (
-        <button className="square" onClick={() => props.onClick(i)}>
-            {props.squares[i]}
-        </button>
-    )
-}
